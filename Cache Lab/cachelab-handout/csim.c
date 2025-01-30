20c354c773597aad1848cecd89ec13f65e4f1cc2
G3 <purep@qq.com>
2025-01-30 16:11:50 +0800

    cache1可以解析参数

--- Cache Lab/cachelab-handout/csim.c
+++ Cache Lab/cachelab-handout/csim.c
@@ -1,7 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
 #include "cachelab.h"
 
-int main()
+int main(int argc, char *argv[])
 {
+    int opt;
+    int verbose = 0;
+    int set_index_bits = 0, lines_per_set = 0, block_offset_bits = 0;
+    char *tracefile = NULL;
+
+    // 解析命令行参数
+    // getopt 函数会从 argv 中提取命令行参数。你通过指定参数字符串来告诉它需要处理的选项。
+    // "hv:s:E:b:t:" 表示：
+    // -h 和 -v 是标志选项，不需要附加值；
+    // -s, -E, -b, 和 -t 后面需要一个值，optarg 会指向该值。
+    while ((opt = getopt(argc, argv, "hv:s:E:b:t:")) != -1)
+    {
+        switch (opt)
+        {
+        case 'h':
+            // 打印帮助信息
+            printf("Usage: ./program [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
+            printf(" -h: Help flag\n");
+            printf(" -v: Verbose mode\n");
+            printf(" -s <s>: Set index bits (S = 2^s)\n");
+            printf(" -E <E>: Number of lines per set (associativity)\n");
+            printf(" -b <b>: Block bits (B = 2^b)\n");
+            printf(" -t <tracefile>: Name of the Valgrind trace file\n");
+            exit(0);
+        case 'v':
+            // 启用详细模式
+            verbose = 1;
+            break;
+        case 's':
+            // 解析集合索引位数
+            set_index_bits = atoi(optarg);
+            break;
+        case 'E':
+            // 解析每个集合的行数（组关联度）
+            lines_per_set = atoi(optarg);
+            break;
+        case 'b':
+            // 解析块位数
+            block_offset_bits = atoi(optarg);
+            break;
+        case 't':
+            // 解析追踪文件
+            tracefile = optarg;
+            break;
+        default:
+            // 处理未知选项
+            fprintf(stderr, "Usage: %s [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n", argv[0]);
+            exit(1);
+        }
+    }
+
+    // 打印解析的参数值
+    printf("Verbose mode: %s\n", verbose ? "Enabled" : "Disabled");
+    printf("Set index bits: %d, Lines per set: %d, Block offset bits: %d, Tracefile: %s\n", 
+            set_index_bits, lines_per_set, block_offset_bits, tracefile);
     printSummary(0, 0, 0);
+    // 在这里可以继续根据解析的参数执行后续的操作
+
     return 0;
 }
