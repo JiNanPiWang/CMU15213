2c4ea9623523fb626e935f8e7dbdee8f6fc92342
Samsara <68888258+JiNanPiWang@users.noreply.github.com>
2025-02-10 02:09:02 +0000

    add run shell

---
+++ Shell Lab/run_shell.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+# /workspaces/CMU15213/run_shell.sh
+
+cd '/workspaces/CMU15213/Shell Lab/shlab-handout'
+
+make clean
+make
+# make test01
+# make rtest01
+# echo
+# echo
+# make test02
+# make rtest02
+# echo
+# echo
+# make test03
+# make rtest03
+# echo
+# echo
+# make test04
+# make rtest04
+# echo
+# echo
+# make test05
+# make rtest05
+# echo
+# echo
+# make test06
+# make rtest06
+# echo
+# echo
+# make test07
+# make rtest07
+# echo
+# echo
+# make test08
+# make rtest08
+# echo
+# echo
+make test09
+make rtest09
+echo
+echo
+make test10
+make rtest10
+echo
+echo
